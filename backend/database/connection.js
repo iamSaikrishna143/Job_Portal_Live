b728b95d4f9573289520262e64a3d66540961c2c
saikrishna <askachary2@gmail.com>
2024-09-28 12:12:17 +0530

    sahsa

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,12 +1,20 @@
-import mongoose from "mongoose";
+https://www.youtube.com/playlist?list=PLwGdqUZWnOp0ZB4HoeV7j2VryoHs7so2i
 
-export const connection =()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName:"JOB_PORTAL_MERN"
-    }
-    ).then(()=>{
-        console.log("Connected to MongoDB");
-    }).catch(err=>{
-        console.log(`Some Error occured while connecting to database: ${err}`);
-    })
-}
+
+https://www.youtube.com/playlist?list=PLAgJNt0flqKduy3VuZbR76f8pMxGjz33X
+
+https://www.youtube.com/playlist?list=PLAx7-E_inM6EkgZkrujZvewiM_QZRU4A2
+
+
+https://www.youtube.com/playlist?list=PLp50dWW_m40UwtiT-sWJ2ktGcLaNXNG9F
+
+
+https://www.youtube.com/playlist?list=PLENVRtTDG0ocMlPIxNjHfZo9P0_Pif73O
+
+https://www.youtube.com/playlist?list=PLpc_YvcwbxaSn6jn0VaTcG8A0Grgs1GSB
+
+
+https://www.youtube.com/playlist?list=PL6K9UwOFzuZg_cRAqP2QqCvxQDR6FnEMh
+
+
+https://www.youtube.com/playlist?list=PLrClazTqVpJlyey7Szwe_XX9meD4wl2Ma
